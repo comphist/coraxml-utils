6bea1bce61469047981be4e6f4dcd51e5f8b0abc
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2017-11-22 18:02:20 +0100

    Add tests for importer factory

---
+++ test/importer/test_factory.py
@@ -0,0 +1,15 @@
+import unittest
+
+from coraxml_utils.importer import create_importer
+
+class ImporterFactoryTest(unittest.TestCase):
+
+    def test_unsupported_file_format(self):
+
+        with self.assertRaises(ValueError):
+            create_importer('"some unknown format"')
+
+    def test_coraxml_unsupported_dialect(self):
+
+        with self.assertRaises(ValueError):
+            create_importer('CorA-XML', '"some unknown dialect"')
