db234f1ad900a843a7ccdc1cf3ac3dbff89651a6
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2017-11-28 15:18:14 +0100

    Add import of ElementTree to exporter

--- coraxml_utils/exporter.py
+++ coraxml_utils/exporter.py
@@ -4,6 +4,11 @@ import logging
 from coraxml_utils.coralib import *
 from coraxml_utils.settings import *
 
+try:
+    from lxml import etree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
 def create_exporter(format="coraxml", dialect="ref"):
     if format == "coraxml":
         return CoraXMLExporter(dialect)
@@ -24,22 +29,21 @@ class CoraXMLExporter:
             self.dipl_tag = "dipl"
             self.mod_tag = "mod"
 
-
     def export(self, doc):
 
-        root = etree.Element("text")
+        root = ET.Element("text")
         root.set("id", doc.sigle)
-        header = etree.SubElement(root, "header")
+        header = ET.SubElement(root, "header")
         if isinstance(doc.header, str):
             header.text = doc.header
-        elif isinstance(doc.header, etree.Element):
+        elif isinstance(doc.header, ET.Element):
             header.append(doc.header)
         else:
             logging.warning("Found something weird in document header")
 
-        layoutinfo = etree.SubElement(root, "layoutinfo")
+        layoutinfo = ET.SubElement(root, "layoutinfo")
         for page in doc.pages:
-            page_xml = etree.Element("page", {"id": page.id,
+            page_xml = ET.Element("page", {"id": page.id,
                                               "no": page.no,
                                               "range": page.range()})
             if page.side:
@@ -47,7 +51,7 @@ class CoraXMLExporter:
             layoutinfo.append(page_xml)
 
             for col in page.columns:
-                col_xml = etree.Element("column", {"id": col.id,
+                col_xml = ET.Element("column", {"id": col.id,
                                                    "range": col.range()})
                 if col.name:
                     col_xml.set("name", self.name)
@@ -57,29 +61,30 @@ class CoraXMLExporter:
                     # empty lines could come about after double dashes at
                     # line end have been resolved
                     if line:
-                        line_xml = etree.Element("line", {"id": self.id,
+                        line_xml = ET.Element("line", {"id": self.id,
                                                           "name": self.linename,
                                                           "loc": self.loc(),
                                                           "range": self.range()})
                         layoutinfo.append(line_xml)
 
-        shifttags = etree.SubElement(root, "shifttags")
+        shifttags = ET.SubElement(root, "shifttags")
         for shifttag in doc.shifttags:
-            etree.SubElement(shifttags, shifttag.tag(), {"range": shifttag.range()})
+            ET.SubElement(shifttags, shifttag.tag(), {"range": shifttag.range()})
 
         for token_or_comment in doc.tokens:
             if isinstance(token_or_comment, CoraToken):
                 tok = token_or_comment
-                tok_xml = etree.Element("token", {"id": tok.id,
+
+                tok_xml = ET.Element("token", {"id": tok.id,
                                                   "trans": tok.trans})
 
                 for dipl in tok.tok_dipls:
-                    dipl_xml = etree.SubElement(tok_xml, self.dipl_tag, 
+                    dipl_xml = ET.SubElement(tok_xml, self.dipl_tag,
                                                 {"id": dipl.id, 
                                                  "trans": str(dipl.trans)})
                     dipl_xml.set("utf", str(dipl.trans.with_opts(Options(character="utf"))))
                 for mod in tok.tok_annos:
-                    mod_xml = etree.SubElement(tok_xml, self.mod_tag,
+                    mod_xml = ET.SubElement(tok_xml, self.mod_tag,
                                                {"id": mod.id,
                                                 "trans": str(mod.trans)})
                     mod_xml.set("utf", str(mod.trans.with_opts(Options(character="utf"))))
@@ -90,7 +95,7 @@ class CoraXMLExporter:
                 root.append(tok_xml)
             elif isinstance(token_or_comment, Comment):
                 comment = token_or_comment
-                comm_xml = etree.Element("comment", {"type": comment.type})
+                comm_xml = ET.Element("comment", {"type": comment.type})
                 comm_xml.text = comment.content
                 root.append(comm_xml)
             else:
