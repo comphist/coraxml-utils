5c55860bb6f29951059a0b509cd1cc1f012cf91d
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2017-12-01 09:00:07 +0100

    Add converter script (CorA-XML to GATE JSON)

---
+++ bin/coraxml2gatejson.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import argparse
+import json
+
+from coraxml_utils.importer import create_importer
+from coraxml_utils.exporter import create_exporter
+
+if __name__ == "__main__":
+    description = "Konvertiert eine CorA-XML-Datei ins GATE JSON-Format."
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('infile',
+                        help='Eingabedatei (XML)')
+    parser.add_argument('outfile', nargs="?",
+                        help='Ausgabedatei (JSON)')
+    ## TODO
+    parser.add_argument("-d", "--dialect", choices=["rem", "anselm", "ref", "redi"],
+                        default=None, help="CorA-XML dialect to use, default: %(default)s")
+    args, _ = parser.parse_known_args()
+
+    MyImporter = create_importer("coraxml", args.dialect)
+    MyExporter = create_exporter("gatejson")
+
+    doc = MyImporter.import_from_file(args.infile)
+    json_doc = MyExporter.export(doc)
+    json.dump(json_doc, open(args.outfile, 'w'))
