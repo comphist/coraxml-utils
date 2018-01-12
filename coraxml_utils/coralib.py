a395c93c0ade9e070461564f3c2353ebb1709c88
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2018-01-12 14:59:07 +0100

    Add AnnoSpan to model

--- coraxml_utils/coralib.py
+++ coraxml_utils/coralib.py
@@ -259,8 +259,14 @@ class TokDipl:
     def __eq__(self, other):
         return (self.id == other.id) and (self.trans == other.trans)
 
+class AnnotatableElement:
 
-class TokAnno:
+    def __init__(self, tags=None, flags=None):
+
+        self.tags = tags if tags else dict()
+        self.flags = flags if flags else set()
+
+class TokAnno(AnnotatableElement):
 
     ## TODO: move to coraxml_exporter, dialect="rem"
     # annos_order = ["norm", "token_type", "lemma", "lemma_gen", "lemma_idmwb",
@@ -273,9 +279,8 @@ class TokAnno:
         self._id = "a{0}".format(TokAnno.c)
         self.id = extid if extid else self._id
         self.trans = trans
-        self.tags = tags if tags else dict()
-        self.flags = flags if flags else set()
         self.checked = checked
+        super().__init__(tags=tags, flags=flags)
 
     def __str__(self):
         return str(self.trans)
@@ -291,6 +296,11 @@ class TokAnno:
     def merge(self, other):
         self.trans.parse += other.trans.parse
 
+class AnnoSpan(AnnotatableElement):
+
+    def __init__(self, annos, tags=None, flags=None):
+        self.annos = annos
+        super().__init__(tags=tags, flags=flags)
 
 class CoraComment:
 
