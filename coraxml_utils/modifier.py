5a94d300d092618a2b084d15cbb3cbcb8bd37e10
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2018-01-30 12:46:53 +0100

    Rename add_val -> _add_val

--- coraxml_utils/modifier.py
+++ coraxml_utils/modifier.py
@@ -1,7 +1,7 @@
 
 from coraxml_utils.settings import DEFAULT_VAL
 
-def add_val(instr, newval, sep=' '):
+def _add_val(instr, newval, sep=' '):
     if instr != DEFAULT_VAL:
         return (instr + sep + newval).strip()
     else:
@@ -18,36 +18,36 @@ def add_tokenization_tags(token):
 
         # multiverbation
         if switch_ML:
-            token_type = add_val(token_type, "ML" + c)
+            token_type = _add_val(token_type, "ML" + c)
             switch_ML = False
             c += 1
         elif switch_MS:
-            token_type = add_val(token_type, "MS" + c)
+            token_type = _add_val(token_type, "MS" + c)
             switch_MS = False
             c += 1
         elif any(c["trans"] == "=|" and c["type"] == "spl" 
                  for c in mparse):
             switch_ML = True
-            token_type = add_val(token_type, "ML1")
+            token_type = _add_val(token_type, "ML1")
             c += 1
         elif any(c["trans"] == "|" and c["type"] == "spl" 
                  for c in mparse):
             switch_MS = True
-            token_type = add_val(token_type, "MS1")
+            token_type = _add_val(token_type, "MS1")
             c += 1
 
         # univerbation
         if any(c["type"] == "dd" for c in mparse):
-            token_type = add_val(token_type, 'UH')
+            token_type = _add_val(token_type, 'UH')
         elif any(c["trans"] == "#" and c["type"] == "spl" 
                  for c in mparse):
-            token_type = add_val(token_type, 'US')
+            token_type = _add_val(token_type, 'US')
         elif any(c["trans"] == "~(=)" and c["type"] == "spl" 
                  for c in mparse):
             pass
         elif any(c["trans"] == "(=)" and c["type"] == "spl" 
                  for c in mparse):
-            token_type = add_val(token_type, 'UL')
+            token_type = _add_val(token_type, 'UL')
 
         if token_type and token_type != DEFAULT_VAL:
             m.tags["token_type"] = token_type
