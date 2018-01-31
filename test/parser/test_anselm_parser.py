d97c4f6d34fde7100608153e1ce6afd98d3b6e3b
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2018-01-31 07:23:40 +0100

    Add helper function to anselm parser test

--- test/parser/test_anselm_parser.py
+++ test/parser/test_anselm_parser.py
@@ -5,13 +5,16 @@ from coraxml_utils.parser import *
 
 class ParserTest(unittest.TestCase):
 
+    def _create_anselm_parse(test_string):
+
+        tok = AnselmParser().parse(test_string)
+        return tok
+
     def test_pro_error(self):
         """Make sure that not all p's are simplified to pro"""
 
-        test_string = "zer$panten"
-        tok = AnselmParser().parse(test_string)
+        tok = ParserTest._create_anselm_parse("zer$panten")
         self.assertEquals(tok.tokenize_anno()[0].simple(), "zerspanten")
 
-        test_string = "enpei$$en"
-        tok = AnselmParser().parse(test_string)
+        tok = ParserTest._create_anselm_parse("enpei$$en")
         self.assertEquals(tok.tokenize_anno()[0].simple(), "enpeissen")
