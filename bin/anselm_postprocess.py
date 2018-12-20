151065d3c382e38d021462dc58fcedf3360f0e5f
Adam Roussel <adam.roussel@rub.de>
2018-12-20 12:30:00 +0100

    ditto

--- bin/anselm_postprocess.py
+++ bin/anselm_postprocess.py
@@ -3,7 +3,7 @@
 
 from coraxml_utils.importer import create_importer
 from coraxml_utils.exporter import create_exporter
-from coraxml_utils.modifier import postprocess, anselm_postprocess, anselm_correct_tokenization
+from coraxml_utils.modifier import postprocess, anselm_postprocess, anselm_document_postprocess
 
 
 if __name__ == "__main__":
@@ -16,5 +16,5 @@ if __name__ == "__main__":
             'anno_tag_name': 'tok_anno',
         }),
         anselm_postprocess,
-        anselm_correct_tokenization
+        anselm_document_postprocess
     )
